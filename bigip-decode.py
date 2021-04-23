1b2daffe9eecebbf0b699f921414591ce22d2ea6
m00np1e <73180416+m00np1e@users.noreply.github.com>
2021-04-23 10:33:36 -0500

    Add files via upload

--- bigip-decode.py
+++ bigip-decode.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
+
 from optparse import OptionParser
 from sys import exit
+
 
 # Decodes F5 BigIP cookies
 # Based on instructions at https://support.f5.com/csp/article/K6917
@@ -16,6 +19,7 @@ def get_port(c_port) -> str:
     r_port2 = "{0}".format((r_port.replace('0x', '')))
     return str(int(r_port2, 16))
 
+
 def get_host(c_host) -> str:
     # convert the first part of the cookie to hex
     hh_host = (hex((int(c_host)))[2:])
@@ -28,15 +32,17 @@ def get_host(c_host) -> str:
     # print out the ip address
     return '.'.join([str(octet) for octet in xhosts])
 
+
 def reverse_bytes(payload) -> str:
     return "".join(reversed([payload[i:i + 2] for i in range(0, len(payload), 2)]))
+
 
 def main():
     parser = OptionParser()
     parser.add_option("-c", "--cookie", type="string")
     (options, args) = parser.parse_args()
     if not options.cookie:
-        parser.error("Cookie is not provided")
+        parser.error("Cookie not provided. Please provide the F5 BigIP cookie.")
         exit(1)
     # initial value of cookie
     cookie = options.cookie
@@ -46,7 +52,8 @@ def main():
     [c_host, c_port] = cookie.split('.')[:2]
     host = get_host(c_host)
     port = get_port(c_port)
-    print("Converted: {0}".format(str(":".join([host, port]))))
+    print("Decoded cookie (IP address:Port): {0}".format(str(":".join([host, port]))))
+
 
 if __name__ == '__main__':
     main()
